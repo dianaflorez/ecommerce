7bd5872eaefd6322603ffffe34737cd500991eff
Diana Cristy Florez Bravo <dianaflorezbravo@gmail.com>
2026-02-05 20:35:30 -0500

    [Fix] empty use html

--- schema_ins.sql
+++ schema_ins.sql
@@ -1,14 +1,14 @@
 
 INSERT INTO product (image,name,description,price,is_initial_kit,active)
 VALUES 
-('agua1.png','Alcaline Max 1','Quando a água deixa de ser comum e se torna biohacking,.',  500, true,true),
-('agua2.png','Alcaline Max 2','Quando a água deixa de ser comum e se torna biohacking,.', 500, true,true),
-('agua3.png','Alcaline Max 3','Quando a água deixa de ser comum e se torna biohacking,.', 500, true,true),
+('agua1.png','Alcaline Max Squeeze','A Alcaline Squeeze resgata a essência da natureza em sua água, melhora o PH Orgânico, auxilia no processo de detox do organismo e aumentam os elétrons na água.',  333, true,true),
+('agua2.png','Purificador Alcaline Max ','Filtrado de Alta Tecnología: Utiliza un refil de 5 capas (minerales, carbón activado con plata coloidal, esferas alcalinas, carbón activado y cuarzo) para purificar y mejorar la calidad del agua.', 500, true,true),
+('agua3.png','Alcaline Max Squeeze 2','La Squeeze Alcaline Max transforma el agua en una fuente de energía, bienestar y autocuidado. ✔️ Menos cloro ✔️ Más minerales ✔️ pH', 350, true,true),
 
-('kit1.png','Starter Kit 1','A complete starter kit for beginners.', 1000, true,true),
+('kit1.png','Starter Kit Bienestar','A complete starter kit for beginners. Squeeze Alcaline + Plantillas', 1300, true,true),
 ('kit2.png','Starter Kit 2','A complete starter kit for beginners.', 1000, true,true),
-('kit3.png','Starter Kit 3','A complete starter kit for beginners.', 1000, true,true),
+('kit3.png','Starter Kit 3','A complete starter kit for beginners. Pulsera + Plantillas', 1000, true,true),
 
-('pulsera2.webp','Pulseira Bracelete 2','Pulseira Bracelete Terapêutico Magnético com Fir Original Novo.', 500, true,true),
-('pulsera3.webp','Pulseira Bracelete 3','Pulseira Bracelete Terapêutico Magnético com Fir Original Novo.', 500, true,true),
-('pulsera.webp','Pulseira Bracelete 1','Pulseira Bracelete Terapêutico Magnético com Fir Original Novo.', 500, true,true);
+('pulsera2.webp','Pulseira Bracelete Negra/Azul','Pulseira Bracelete Terapêutico Azul Magnético com Fir Original Novo.', 500, true,true),
+('pulsera3.webp','Pulseira Bracelete Rosa/Gris','Pulseira Bracelete Terapêutico Gris - Rosa Magnético com Fir Original Novo. La pulsera Nipponflex (e-Energy) es un accesorio terapéutico de diseño moderno, fabricado en silicona de alta calidad con hebilla ajustable, que integra tecnologías FIR Power (infrarrojo lejano), imanes de ferrita y esferas de iones negativos. Está diseñada para mejorar la circulación sanguínea, el equilibrio, la flexibilidad y ayudar a aliviar el dolor. ', 500, true,true),
+('pulsera.webp','Pulseira Bracelete Negra/Roja','Pulseira Bracelete Terapêutico Negra - Roja Magnético com Fir Original Novo. La pulsera Nipponflex (e-Energy) es un accesorio terapéutico de diseño moderno, fabricado en silicona de alta calidad con hebilla ajustable, que integra tecnologías FIR Power (infrarrojo lejano), imanes de ferrita y esferas de iones negativos. Está diseñada para mejorar la circulación sanguínea, el equilibrio, la flexibilidad y ayudar a aliviar el dolor. ', 500, true,true);
