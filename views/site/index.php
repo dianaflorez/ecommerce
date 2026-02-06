beff941a61631ccbbceff7c9a5f1b8acd692b245
Diana Cristy Florez Bravo <dianaflorezbravo@gmail.com>
2026-02-05 23:02:58 -0500

    [ADD] table para referidos

--- views/site/index.php
+++ views/site/index.php
@@ -84,6 +84,51 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>        
 
                 </div>
+
+                <div class="col-lg-6 col-md-12">
+                    <div class="card">
+                        <div class="card-header card-header-warning">
+                        <h4 class="card-title">Ultimas Ventas</h4>
+                        <p class="card-category">Ventas realizadas Febrero, 2026</p>
+                        </div>
+                        <div class="card-body table-responsive">
+                        <table class="table table-hover">
+                            <thead class="text-warning">
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Salary</th>
+                            <th>Country</th>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Dakota Rice</td>
+                                <td>$36,738</td>
+                                <td>Brazil</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Minerva Hooper</td>
+                                <td>$23,789</td>
+                                <td>Mexico</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Sage Rodriguez</td>
+                                <td>$56,142</td>
+                                <td>Ecuador</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Philip Chaney</td>
+                                <td>$38,735</td>
+                                <td>Colombia</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                    </div>
             </div>
 
         <!-- DISTRIBUIDOR -->
